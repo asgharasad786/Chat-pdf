056c7f66e8d27d5b5f2dfc3bbde068cbad6a2ebd
asgharasad786 <161506651+asgharasad786@users.noreply.github.com>
2024-11-04 13:10:17 +0500

    htmlTemplates.py

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -3,6 +3,7 @@ css = '''
 /* Global styling for the entire screen */
 body {
     background-color: #303030; /* Dark grey background */
+    color: black;s
     color: white !important; /* White text color */
     font-family: 'Arial', sans-serif; /* Font family */
 }
